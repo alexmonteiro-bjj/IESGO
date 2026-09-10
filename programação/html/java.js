9259d92c48c4d6f3822f63e6ca724a1e4bdd95a8
ALEX <alex206monteiro@gmail.com>
2026-09-10 15:10:45 -0300

    tudo novo 2

--- programação/html/java.js
+++ programação/html/java.js
@@ -1,29 +1,16 @@
-const elementos = document.querySelectorAll(".animacao, .card");
+const elementos = document.querySelectorAll(".animacao");
 
-const observador = new IntersectionObserver(
-    (entradas) => {
-
-        entradas.forEach((entrada) => {
-
-            if (entrada.isIntersecting) {
-
-                entrada.target.classList.add("aparecer");
-
-            }
-
-        });
-
-    },
-
-    {
-        threshold: 0.6
-    }
-
-);
-
+const observador = new IntersectionObserver((entradas) => {
+entradas.forEach((entrada) => {
+if(entrada.isIntersecting){
+entrada.target.classList.add("aparecer");
+observador.unobserve(entrada.target);
+}
+});
+},{
+threshold:0.15
+});
 
 elementos.forEach((elemento) => {
-
-    observador.observe(elemento);
-
+observador.observe(elemento);
 });
